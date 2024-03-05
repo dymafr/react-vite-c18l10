0319a81fe551ed02d0902e5cbe19561359206cdf
Erwan Vallée <erwanp.vallee@gmail.com>
2024-03-05 14:58:46 +0100

    maj

--- src/data/seed.js
+++ src/data/seed.js
@@ -1,13 +1,13 @@
-import { data } from './recipes';
+import { data } from "./recipes";
 
 export async function seedRecipes() {
-  const recipes = await fetch('https://restapi.fr/api/recipes');
+  const recipes = await (await fetch("https://restapi.fr/api/recipes")).json();
 
-  if (recipes.length < 10) {
-    await fetch('https://restapi.fr/api/recipes', {
-      method: 'POST',
+  if (recipes.length < 20) {
+    await fetch("https://restapi.fr/api/recipes", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
